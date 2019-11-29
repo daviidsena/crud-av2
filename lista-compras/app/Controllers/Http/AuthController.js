a3e909a12266649540ef72defb4bc4cc96eabfea
David Sena <david.sena@geohub.com.br>
2019-11-29 01:05:01 -0200

    Flag para Intellisense funcionar no Adonis

--- lista-compras/app/Controllers/Http/AuthController.js
+++ lista-compras/app/Controllers/Http/AuthController.js
@@ -1,12 +1,13 @@
 'use strict'
 
+// Flag para Intellisence funcionar com Adonis  https://blog.adonisjs.com/adonis-intellisence/
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const User = use('App/Models/User')
 
 class AuthController {
   async register({ request }) {
     const data = request.only(['email', 'password', 'username']);
-    
+
     console.log(request);
 
     const user = await User.create(data);
